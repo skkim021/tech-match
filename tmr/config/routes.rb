1aedcf9f2458d40cedfd47dd8803cac1b48e9b4d
Laurie415 <Laurie@awesome>
2014-08-19 20:14:12 -0700

    model user with attributes

--- tmr/config/routes.rb
+++ tmr/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  get 'users' => 'users#index'
+  get 'users/new' => 'users#new', as: :new_user
+  post 'users' => 'users#create'
+  get 'users/:id' => 'users#show', as: :ticket
+  get 'users/:id/edit' => 'users#edit', as: :edit_user 
+  patch 'users/:id' => 'users#update', as: :show_user
+  delete 'users/:id' => 'users#destroy', as: :delete_user
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
