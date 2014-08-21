8f2aecaecc44ac746e82843faf08c95a4d6dc08a
angelali92 <angela@li>
2014-08-20 19:12:06 -0700

    saving script changes. Will add call with JSON file later

--- tma/script.js
+++ tma/script.js
@@ -1,30 +1,21 @@
 $( document ).ready(function(){
 
-	// python -m SimpleHTTPServer 8000
-	// Begin our AJAX Stuff
-	// Define our secret message as a variable and initiatee ajax call
+	// python -m SimpleHTTPServer 8000l
 
-	// Second HTML Page
+	// AJAX call using separate html file.
 	var ourMessage = $.ajax("/test.html")
 	.done(function(){
-		// If ajax requet done, alert success
 		console.log('success');
-		// responseText is something jquery Ajax gives us to return the string of our html object
-		// console.log(ourMessage.responseText);
 	})
 	.fail(function(){
-		// If ajax request fails, alert error
 		alert('error');
 	});
 
-	// Define dynamic calls
 	$(".angela").click(function(){
-
-		// $(".angela").load("/test.html .hidden", function(){
-		// });
 		console.log("inside click function");
 		$( this ).load("/test.html .hiddenM");
-
 	});
+
+	// AJAX call using 
 
 });
